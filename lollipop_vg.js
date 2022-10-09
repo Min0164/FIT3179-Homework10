16d1da471dfe62d3b5791168cd617fd791e368f4
Min0164 <96381348+Min0164@users.noreply.github.com>
2022-10-09 20:35:08 +0800

    uploaded files

---
+++ lollipop_vg.js
@@ -0,0 +1,4 @@
+var spec2 = "lollipop_chart.json";
+vegaEmbed("#lollipop_chart", spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error)
---
+++ map_vg.js
@@ -0,0 +1,5 @@
+var spec2 = "empty_data_map.json";
+vegaEmbed("#choropleth_map", spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error)
+
